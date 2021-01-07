2d46933ef4413af654b4ec9b745f31973caabdcb
mlisle <mlisle@protonmail.com>
2021-01-06 16:59:02 -0800

    Add User auth on models

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,9 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Style, Accessory
 from .forms import WearingForm
@@ -11,17 +14,17 @@ from .forms import WearingForm
 from django.http import HttpResponse
 
 
-class StyleCreate(CreateView):
+class StyleCreate(LoginRequiredMixin, CreateView):
     model = Style
     fields = ['title', 'brand', 'description', 'era']
     success_url = '/styles/'
 
-class StyleUpdate(UpdateView):
+class StyleUpdate(LoginRequiredMixin, UpdateView):
     model = Style
     fields = ['title', 'brand', 'description', 'era']
     
 
-class StyleDelete(DeleteView):
+class StyleDelete(LoginRequiredMixin, DeleteView):
     model = Style
     success_url = '/styles/'       
 
@@ -32,10 +35,12 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def styles_index(request):
     styles = Style.objects.all()
     return render(request, 'styles/index.html', { 'styles': styles })
 
+@login_required
 def styles_detail(request, style_id):
     style = Style.objects.get(id=style_id)
     # instantiate FeedingForm to be rendered in the template
@@ -47,6 +52,7 @@ def styles_detail(request, style_id):
         'accessories': accessories_style_doesnt_have 
     }) 
 
+@login_required
 def add_wearing(request, style_id):
     form = WearingForm(request.POST)
     if form.is_valid():
@@ -55,31 +61,33 @@ def add_wearing(request, style_id):
         new_wearing.save()
     return redirect('detail', style_id=style_id)
 
+@login_required
 def assoc_accessory(request, style_id, accessory_id):
     Style.objects.get(id=style_id).accessories.add(accessory_id)
     return redirect('detail', style_id=style_id)
 
+@login_required
 def unassoc_accessory(request, style_id, accessory_id):
     Style.objects.get(id=style_id).accessories.remove(accessory_id)
     return redirect('detail', style_id=style_id)    
 
-class AccessoryList(ListView):
+class AccessoryList(LoginRequiredMixin, ListView):
     model = Accessory
    
-class AccessoryDetail(DetailView):
+class AccessoryDetail(LoginRequiredMixin, DetailView):
     model = Accessory    
 
-class AccessoryCreate(CreateView):
+class AccessoryCreate(LoginRequiredMixin, CreateView):
     model = Accessory
     fields = '__all__'
     success_url = '/accessories/'
 
-class AccessoryUpdate(UpdateView):
+class AccessoryUpdate(LoginRequiredMixin, UpdateView):
     model = Accessory
     fields = ['title', 'brand', 'color']
         
 
-class AccessoryDelete(DeleteView):
+class AccessoryDelete(LoginRequiredMixin, DeleteView):
     model = Accessory
     success_url = '/accessories/'
 
